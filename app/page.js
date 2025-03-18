f61720e622bda1a963846d07962a9fab69e39d62
James Holt <jamesphilipdavidholt@gmail.com>
2025-03-18 15:21:01 +0000

    initial

--- app/page.js
+++ app/page.js
@@ -1,103 +1,95 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	return (
+		<div className="">
+			<main className="h-full min-h-[100vh] relative overflow-x-hidden max-w-[100vw]">
+				<Image
+					src="/blob-1.svg"
+					alt="decorative image"
+					width={420}
+					height={420}
+					className="fixed z-10 -top-20 -left-20 w-[200px] lg:-top-40 lg:-left-40 lg:w-[400px]"
+				/>
+				<Image
+					src="/blob-2.svg"
+					alt="decorative image"
+					width={420}
+					height={420}
+					className="fixed -bottom-20 -right-20 z-10 w-[200px] lg:-bottom-40 lg:-right-40 lg:w-[400px]"
+				/>
+				<div className="relative w-full flex flex-col items-center">
+					<Image
+						src="/bg.jpg"
+						alt="mountains"
+						fill={true}
+						className="h-full w-full object-cover object-bottom absolute bottom-0"
+					/>
+					<Image
+						src="/hfj-logo.svg"
+						alt="hope for justice logo"
+						width={283}
+						height={84}
+						className="relative my-8 max-w-[150px] lg:max-w-[200px]"
+					/>
+					<Image
+						src="/extreme-challenge-head.svg"
+						alt="Extreme Challenge 2026 Canada: Sea to Summit. September 12th to 20th"
+						width={773}
+						height={486}
+						className="relative mb-20 w-[80%] max-w-[700px] lg:mb-[200px]"
+					/>
+				</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+				<div className="relative flex flex-col items-center -mt-5 lg:-mt-20 z-20">
+					<Image
+						src="/extreme-challenge-intro.svg"
+						alt="Appreciate the sheer beauty of British Columbia from Vancuver to Whistler while pushing yourself to the limit for an incredible cause"
+						width={1132}
+						height={275}
+						className="mb-10 w-[100vw] max-w-[1000px]"
+					/>
+					<p className="text-sm text-center max-w-[90%] mb-[8vw] lg:max-w-[50%] lg:mb-20 md:text-xl lg:text-2xl">
+						CHANGE THE LIVES OF PEOPLE TRAPPED IN MODERN SLAVERY AND WHO HAVE
+						SURVIVED HUMAN TRAFFICKING.
+					</p>
+					<div className="mx-4 flex gap-5 lg:gap-20 mb-10">
+						<a
+							target="_blank"
+							href="https://hopeforjustice.org/wp-content/uploads/2025/03/Extreme-Challenge-2026-Brochure-A4-Single-Pages.pdf"
+							className="cursor-pointer hover:scale-105 transition-all"
+						>
+							<Image
+								src="/download-button.svg"
+								alt="download"
+								width={465}
+								height={96}
+							/>
+						</a>
+						<a
+							href="mailto:andrew.wrath@hopeforjustice.org"
+							className="cursor-pointer hover:scale-105 transition-all"
+						>
+							<Image
+								src="/get-in-touch-button.svg"
+								alt="get in touch"
+								width={309}
+								height={96}
+							/>
+						</a>
+					</div>
+					<p className="mb-40 px-4 text-center">
+						Email organiser{" "}
+						<a
+							className="underline"
+							href="mailto:andrew.wrath@hopeforjustice.org"
+						>
+							andrew.wrath@hopeforjustice.org
+						</a>{" "}
+						with your questions or to request an application form
+					</p>
+				</div>
+			</main>
+		</div>
+	);
 }
